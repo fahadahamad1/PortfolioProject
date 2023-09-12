38ea9f7c9d6934f4ce7db56b7d3c477ed45083ff
Fahad Ahamad <124129991+fahadahamad1@users.noreply.github.com>
2023-09-12 14:24:59 +0530

    Atliq_sales_project.sql

--- Atliq_sales_project.sql
+++ Atliq_sales_project.sql
@@ -1,49 +1,91 @@
-select * from customers;
+-- All Tables
 
-select * from date;
+SELECT * FROM customers;
 
-select * from markets;
+SELECT * FROM date;
 
-select * from products;
+SELECT * FROM markets;
 
-select * from transactions;
+SELECT * FROM products;
 
-alter table customers change custmer_name customer_name varchar(50);
+SELECT * FROM transactions;
 
-## find Top 5 customer name  by amount
+-- Rename the Column Name 
 
-select c.customer_name , sum(t.sales_amount) as Total_Amount from customers c
-join transactions t
-on c.customer_code = t.customer_code
-group by customer_name
-order by Total_Amount desc
-limit 5;
+ALTER TABLE customers CHANGE custmer_name customer_name VARCHAR(50);
 
-## find bottom 5 customer name by amount and Qty
+-- find Top 5 customer name  by amount
 
-select c.customer_name , sum(t.sales_amount) as Total_Amount , sum(t.sales_qty) as Total_Qty from customers c
-join transactions t
-on c.customer_code = t.customer_code
-group by customer_name
-order by Total_Amount asc
-limit 5;
+SELECT c.customer_name, SUM(t.sales_amount) AS total_amount 
+FROM customers c 
+JOIN transactions t 
+ON c.customer_code = t.customer_code
+GROUP BY customer_name
+ORDER BY total_amount DESC
+LIMIT 5;
 
-## find customer name , market name , zone , order_date from oldest to newest ,Qty and amount 
+-- find bottom 5 customer name by amount and Qty
 
-select c. customer_name , m.markets_name , m.zone , t.Order_date , t.sales_qty , t.sales_amount from customers c
-join transactions t 
-on c.customer_code = t.customer_code
-join markets m 
-on t.market_code = m.markets_code
-order by t.order_date Asc;
+SELECT c.customer_name, SUM(t.sales_amount) AS total_amount, SUM(t.sales_qty) AS total_qty 
+FROM customers c 
+JOIN transactions t 
+ON c.customer_code = t.customer_code
+GROUP BY customer_name
+ORDER BY total_amount ASC
+LIMIT 5;
 
-## Find Total Amount by zone
+-- find customer name , market name , zone , order_date from oldest to newest ,Qty and amount 
 
-select m.zone , Sum(t.sales_amount) as Total_Amount from markets m
-join transactions t
-on m.markets_code = t.market_code
-group by zone 
-order by Total_Amount Desc;
+SELECT c.customer_name, m.markets_name, m.zone, t.order_date, t.sales_qty, t.sales_amount
+FROM customers c
+JOIN transactions t 
+ON c.customer_code = t.customer_code
+JOIN markets m
+ON t.market_code = m.markets_code
+ORDER BY t.order_date ASC;
+
+-- Find Total Amount by zone
+
+SELECT m.zone, SUM(t.sales_amount) AS total_amount 
+FROM markets m 
+JOIN transactions t 
+ON m.markets_code = t.market_code
+GROUP BY zone
+ORDER BY total_amount DESC;
+
+-- find customer name, customer type , date , cy_date , market name , product type , order date , sales Qty and sales amount
+
+SELECT c.customer_name , c.customer_type , p.product_type,m.markets_name , d.cy_date , t.order_date , t.sales_qty , t.sales_amount 
+FROM customers c
+JOIN transactions t
+ON c.customer_code = t.customer_code
+JOIN products p 
+ON p.product_code = t.product_code
+JOIN markets m
+ON m.markets_code = t.market_code
+JOIN date d 
+ON d.date = t.order_date
+ORDER BY cy_date ASC; 
+
+-- find Total Amount for Own Brand
+
+SELECT p.product_type , SUM(t.sales_amount) AS total_amount 
+FROM products p
+JOIN transactions t 
+ON p.product_code = t.product_code
+GROUP BY product_type;
+
+-- find the total amount by product type 
+
+SELECT p.product_type , SUM(t.sales_amount) AS Total_Amount 
+FROM products p
+JOIN transactions t 
+ON p.product_code = t.product_code
+GROUP BY product_type; 
+
+
+DELETE FROM transactions
+WHERE sales_amount = 0;
 
 
 
